a82246b2ae34c0a5ec64f9da3212faa97f59d59c
hijkzzz <janhu9527@gmail.com>
2019-11-15 16:51:55 +0800

    fix

--- src/optim/distributed_adam.py
+++ src/optim/distributed_adam.py
@@ -7,11 +7,9 @@ import torch.distributed as dist
 # inherit Adam optim from torch library
 class DistributedAdam(optim.Adam):
 
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0, comm=None):
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
         # import all the basic params in Adam class
         super(DistributedAdam, self).__init__(params, lr, betas, eps, weight_decay)
-        # MPI comm
-        self.comm = comm
 
     def step(self): # use super(SharedAdam, self).step
         size = float(dist.get_world_size())
